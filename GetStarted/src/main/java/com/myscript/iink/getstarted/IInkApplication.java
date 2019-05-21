77e9737ed5f15b97303743372af18b610ff352e7
Anchorer <anchorer.du@gmail.com>
2019-05-21 20:32:10 +0800

    feat: set text language as zh_CN

--- GetStarted/src/main/java/com/myscript/iink/getstarted/IInkApplication.java
+++ GetStarted/src/main/java/com/myscript/iink/getstarted/IInkApplication.java
@@ -3,13 +3,22 @@
 package com.myscript.iink.getstarted;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.myscript.certificate.MyCertificate;
+import com.myscript.iink.Configuration;
 import com.myscript.iink.Engine;
 
 public class IInkApplication extends Application
 {
   private static Engine engine;
+  private static Context context;
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    context = this;
+  }
 
   public static synchronized Engine getEngine()
   {
@@ -17,6 +26,12 @@ public class IInkApplication extends Application
     {
       engine = Engine.create(MyCertificate.getBytes());
     }
+
+    String configDir = "zip://" + context.getPackageCodePath() + "!/assets/conf";
+    Configuration config = engine.getConfiguration();
+    config.setStringArray("configuration-manager.search-path", new String[]{configDir});
+    config.setString("lang", "zh_CN");
+    config.setString("text.configuration.name", "cur_text");
 
     return engine;
   }
